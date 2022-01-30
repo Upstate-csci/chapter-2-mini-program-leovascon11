987052a9366cde73e3b66238e176d12e46fc0927
leovascon11 <97853186+leovascon11@users.noreply.github.com>
2022-01-29 20:56:11 -0500

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -5,7 +5,25 @@ public class Fraction
 	public static void main(String[] args)
 	{
     // declare a scanner object for input
-		Scanner scan = new 
+		Scanner scan = new Scanner (System.in);
+		
+		double fraction;
+		int fractionValue;
+		int decimal;
+		double value;
+		
+		System.out.print("Enter Numerator: ");
+		int numerator = scan.nextInt();
+		System.out.print("Enter Denominator: ");
+                int denominator = scan.nextInt();
+
+                fraction = (double) numerator / denominator;
+                fractionValue = (int) (fraction * 10);
+                decimal = fractionValue % 10;
+                value = decimal * 0.1;
+		
+		System.out.print(value);
+
 
     // prompt for numerator and read in - use print instead of println so the user will input on the same line as the prompt
     
